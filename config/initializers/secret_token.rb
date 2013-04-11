2e6077c37fe3f824e6c61f64924695b556498746
javajigi <javajigi@gmail.com>
2013-04-11 19:33:58 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Humhum::Application.config.secret_token = '8562223dbb4fd07da3846b08be6e66928c8b0069e15a1b0b7f22ca7a79aac6ef3789e49eafe46c68be54801d2a57625084ba201f0b7f3ff3326921dec906ccbf'
