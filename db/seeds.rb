e80b7917864657f9ce3485880f6bd23686000259
Dongkuk <dongkuk5411@nhnnext.org>
2013-04-23 13:31:55 +0900

    update db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,7 +8,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-   musics = Music.create([ {user_picture:'http://binaryapi.ap.org/b18aee87a63e4ad1954406ff9b08be2f/460x.jpg', user_name:'PSY', album_picture:'http://cfile9.uf.tistory.com/image/1866934D5003C7F71AB19A', album_title:'Gangnam Style', album_description:'psy psyp syp syp sy psypsypsyp syp syp spy', filename:'gangnam.mp3', ip:'127.0.0.1'} ])
+   musics = Music.create([ {user_picture:'http://binaryapi.ap.org/b18aee87a63e4ad1954406ff9b08be2f/460x.jpg', user_name:'PSY', album_picture:'http://cfile9.uf.tistory.com/image/1866934D5003C7F71AB19A', album_title:'강남스타일', album_description:'오빤 강남스타일 낮에는 따사로운 인간적인 여자 커피 한잔의 여유를 아는 품격 있는 여자 밤이 오면 심장이 뜨거워지는 여자 그런 반전 있는 여자 나는 사나이 낮에는 너만큼 따사로운 그런 사나이 커피 식기도 전에 원샷 때리는 사나이 밤이 오면 심장이 터져버리는 사나이 그런 사나이', filename:'gangnam.mp3', ip:'127.0.0.1'},
+		   {user_picture:'http://sure.joinsmsn.com/component/article_img/2011/09/2714263949321.jpg', user_name:'리쌍', album_picture:'http://cfile235.uf.daum.net/image/183C003B50464A661A2561', album_title:'독기', album_description:'난 독기를 품고 쿵치딱 흐르는 비트 위로 부딪친다 삶의 가르침과 고통을 쓰고 난 행복을 훔친다 Hook] 삶의 늪에서 삶의 늪에서 나는 더 큰 세상을 보네 삶의 끝에서 후회하지 않게 나는 미친듯이 달려 미친듯이 살어', filename:'독기.mp3', ip:'127.0.0.1'},
+		   {user_picture:'http://wstatic.dcinside.com/new/a_new/ahh/201108/iu0829.jpg', user_name:'아이유', album_picture:'http://postfiles12.naver.net/20111209_203/cha476476_13233765679986MvQX_JPEG/2011-12-09_05%3B31%3B46.jpg?type=w2', album_title:'너랑나', album_description:'기다려줘 너랑 나랑은 지금 안되지 시계를 더 보채고 싶지만 네가 있던 미래에서 내 이름을... 건다 너랑 나랑은 조금 남았지 몇날 몇실진 모르겠지만 네가 있을 미래에서 혹시 내가 헤맨다면 너를 알아볼 수 있게 내 이름을 불러줘', filename:'너랑나.mp3', ip:'127.0.0.1'},
+		   {user_picture:'http://postfiles3.naver.net/20120405_50/mysun1201_1333632801012l883Q_JPEG/11.jpg?type=w1', user_name:'버스커버스커', album_picture:'http://postfiles7.naver.net/20120413_166/dfg543_1334296332389W36c6_JPEG/%B9%F6%BD%BA%C4%BF%B9%F6%BD%BA%C4%BF-1.jpg?type=w1', album_title:'벚꽃엔딩', album_description:'그대여 그대여 그대여 그대여 그    대여 오늘은 우리 같이 걸어요 이 거리를 밤에 들려오는 자장노래 어떤가요 ( oh yeah ) 몰랐던 그대와 단 둘이 손 잡고 알 수 없는 이 떨림과 둘이 걸어요 봄바람 휘날리며 흩날리는 벚꽃 잎이', filename:'벚꽃엔딩.mp3', ip:'27.0.0.1'},
+		   {user_picture:'http://cfile221.uf.daum.net/image/177564444FE5BC2502DB10', user_name:'처진달팽이', album_picture:'http://postfiles14.naver.net/20111216_253/rmrudnfcktwl_1323974678024jkg9g_JPEG/%C0%AF21.jpg?type=w1', album_title:'말하는대로', album_description:'두눈을 감아도 통 잠은 안오고 가슴은 아프도록 답답할때 난 왜 안되지 왜 난 안되지 되뇌었지 말하는 대로 말하는 대로 될 수 있다곤 믿지 않았지 믿을 수 없었지 마음먹은 대로 생각한 대로 할 수 있단 건 거짓말 같았지 고개를 저었지 그러던 어느 날 내 맘에', filename:'말하는대로.mp3', ip:'127.0.0.1'}])
 
 
 
