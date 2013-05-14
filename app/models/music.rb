a655b013ec25af778b598ee9cfa2947acacce269
Dongkuk <dongkuk5411@nhnnext.org>
2013-05-15 00:13:09 +0900

    뮤직 모델 테스트코드

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,3 +1,7 @@
 class Music < ActiveRecord::Base
   attr_accessible :user_picture, :user_name, :album_picture, :album_title, :album_description, :filename, :ip
+
+  validates :album_title, :album_picture, :album_description, :user_name,
+  			:presence => true
+
 end
--- test/unit/music_test.rb
+++ test/unit/music_test.rb
@@ -1,7 +1,67 @@
 require 'test_helper'
 
 class MusicTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "the truth" do
+    assert true
+  end
+
+  test "do not save without user picture" do
+  	music = Music.new
+  	music.user_name = "user_name"
+  	music.album_picture = "album_picture"
+  	music.album_title = "album_title"
+  	music.album_description = "album_description"
+  	music.filename = "filename"
+  end
+
+  test "do not save without user name" do
+  	music = Music.new
+  	music.user_picture = "user_picture"
+  	music.album_picture = "album_picture"
+  	music.album_title = "album_title"
+  	music.album_description = "album_description"
+  	music.filename = "filename"
+  end
+
+  test "do not save without album picture" do
+  	music = Music.new
+  	music.user_picture = "user_picture"
+  	music.user_name = "user_name"
+  	music.album_title = "album_title"
+  	music.album_description = "album_description"
+  	music.filename = "filename"
+  end
+
+  test "do not save without album title" do
+  	music = Music.new
+  	music.user_picture = "user_picture"
+  	music.user_name = "user_name"
+  	music.album_picture = "album_picture"
+  	music.album_description = "album_description"
+  	music.filename = "filename"
+  end
+
+  test "do not save without album description" do
+  	music = Music.new
+  	music.user_picture = "user_picture"
+  	music.user_name = "user_name"
+  	music.album_picture = "album_picture"
+  	music.album_title = "album_title"
+  	music.filename = "filename"
+  end
+
+  test "do not save without filename" do
+  	music = Music.new
+  	music.user_picture = "user_picture"
+  	music.user_name = "user_name"
+  	music.album_picture = "album_picture"
+  	music.album_title = "album_title"
+  	music.album_description = "album_description"
+  end
+
 end
+
+
+
+
