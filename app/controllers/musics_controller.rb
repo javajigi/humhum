e89dfca6f74fe7798094addf338401167c7cb184
javajigi <javajigi@gmail.com>
2013-05-30 20:33:33 +0900

    user_picture use facebook profile url

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -48,7 +48,7 @@ class MusicsController < ApplicationController
   def create
     @music = Music.new(params[:music])
     @music.user_name = current_user.username
-    @music.user_picture = 'http://binaryapi.ap.org/b18aee87a63e4ad1954406ff9b08be2f/460x.jpg'
+    @music.user_picture = current_user.user_picture
 
     # params[:person][:picture] 수정
     uploaded_io = params[:music][:filename]
